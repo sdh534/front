0041b4756869a01bb80149bb27e3a3f5ac685b4d
sdh534 <sdh534@gmail.com>
2023-04-11 17:51:05 +0900

    20230411-1750

---
+++ 0412_js/test8.js
@@ -0,0 +1,15 @@
+function atom3(){
+  document.write("이곳은 atom3입니다. : "+ (100+300), "<br/>");
+      
+  console.log("atom3입니다. : "+  (100+300)); 
+
+  alert("이곳은 atom3입니다." + (100+400));
+}
+
+function atom4(){
+  document.write("이곳은 atom4입니다. : "+ (mbc+300), "<br/>");
+      
+  console.log("atom4입니다. : "+  (mbc+300)); 
+
+  alert("이곳은 atom4입니다." + (mbc+400));
+}
